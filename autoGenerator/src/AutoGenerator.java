a2337335e986e60336693e67495d2de8daee3a0e
Khul3n <game.reg0217@gmail.com>
2018-08-26 18:05:19 +0200

    update von autoGenerator

--- autoGenerator/src/AutoGenerator.java
+++ autoGenerator/src/AutoGenerator.java
@@ -1,11 +1,8 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
-import java.util.UUID;
 
 public class AutoGenerator {
 	public static void main(String[] args) {
@@ -30,11 +27,11 @@ public class AutoGenerator {
 		int lenghtt = AutoT.length;
 
 		Random rand = new Random();
-		int kms = rand.nextInt(15000) + 1;
+		int kms = rand.nextInt(150000) + 1;
 		int tks = rand.nextInt(50) + 1;
 		int mdz = rand.nextInt(12) + 1;
 		int kkms = rand.nextInt(10000) + 1;
-		int kmdz = rand.nextInt(8) + 1;
+
 
 		int randa = (int) (Math.random() * lenghta);
 		int randm = (int) (Math.random() * lenghtm);
@@ -53,43 +50,44 @@ public class AutoGenerator {
 		} else {
 			try {
 
-				File file = new File(dir.getAbsolutePath().concat("/" + UUID.randomUUID()).concat(".txt"));
+				File file = new File(dir.getAbsolutePath().concat("/" +("Auto.txt")));
 				PrintWriter pw = new PrintWriter(new FileOutputStream(file));
 
 				pw.println("Sie bekommen ein : " + Auto[randa]);
 
 				if (Auto[randa] == "Volkswagen") {
 					String autom = AutoVW[randvw];
-					System.out.println("Modelle : " + autom);
+					pw.println("Modelle : " + autom);
 				}
 
 				else if (Auto[randa] == "Opel") {
 					String autom = AutoO[rando];
-					System.out.println("Modelle : " + autom);
+					pw.println("Modelle : " + autom);
 				}
 
 				else if (Auto[randa] == "Toyota") {
 					String autom = AutoT[randt];
-					System.out.println("Modelle: " + autom);
+					pw.println("Modelle: " + autom);
 				}
 
 				pw.println("Mit dem Motor : " + Motor[randm]);
 				pw.println("Farbe ist " + Farbe[randf]);
+
+
+				if (Zulassung[randz].equals("2018")) {
+					kms = kkms;
+				}
 				pw.println("Erstzulassung : " + mdz + "." + Zulassung[randz]);
 				pw.println("Mit Hubraum : " + Hubraum[randh]);
 				pw.println("Als " + Variant[randv]);
-
-				if (Zulassung[randz] == "2018") {
-					kms = kkms;
-				}
-
 				pw.println("Das Auto hat " + kms + " KM");
+				;
 
 				if (tks < 20) {
 					tks = 15;
 				}
 				pw.println("Das Auto hat " + tks + "L im Tank");
-				pw.close();
+
 				pw.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
